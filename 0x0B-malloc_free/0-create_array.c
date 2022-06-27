29818ed2361499a090abf5b36ca2d1fae0b57855
Kidus Birhanu <76646467+kidussb@users.noreply.github.com>
2022-06-27 22:46:09 +0300

    Add files via upload
    My first commit on 0x0B-malloc_free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - create an array of chars
+ * @size: size of array
+ * @c: character
+ * Return: char pointer to array or Null if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	a = malloc(size * sizeof(*a));
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		a[i] = c;
+
+	return (a);
+}
